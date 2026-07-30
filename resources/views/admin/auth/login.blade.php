b2adc30ac980e0830377cd1e1ba14528b6f68f9a
Arturio186 <bain.romanoff@mail.ru>
2026-07-31 02:11:59 +0500

    add login

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -1,4 +1,4 @@
-@extends('admin.layouts.app')
+@extends('admin.layouts.login')
 
 @section('title', 'Вход')
 
@@ -6,7 +6,11 @@
 
 <div class="login">
 
-    <form method="POST" class="login-form">
+    <form
+        method="POST"
+        action="{{ route('admin.login') }}"
+        class="login-form"
+    >
 
         @csrf
 
---
+++ resources/views/admin/layouts/login.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0"
+    >
+
+    <title>
+        @yield('title')
+    </title>
+
+
+    <link
+        rel="stylesheet"
+        href="{{ asset('styles/admin-login.css') }}"
+    >
+
+</head>
+
+
+<body>
+
+    @yield('content')
+
+</body>
+
+</html>
